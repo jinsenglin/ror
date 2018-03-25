19da72fec1f4dc374b4d76f55a3baad7395ac16b
Jim Lin <jimlintw922@gmail.com>
2018-03-25 17:51:35 +0800

    mod data_controller.rb (handle more querystring)

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,11 +1,35 @@
 class DataController < ApplicationController
   def index
+    if params[:draw] == nil
+        draw = 1
+    else
+        draw = Integer(params[:draw]) + 1
+    end
+
+    if params[:start] == nil
+        start = 0
+    else
+        start = Integer(params[:start])
+    end
+
+    if params[:length] == nil
+        length = 10
+    else
+        length = Integer(params[:length])
+    end
+
+    if params[:search] == nil
+        search_value = ""
+    else
+        search_value = params[:search][:value]
+    end
+
     data = {
-        "draw": 1,
+        "draw": draw,
         "recordsTotal": 2,
         "recordsFiltered": 2,
         "data": [
-            ["Airi", "Satou", "Accountant", "Tokyo", "28th Nov 08", "$162,700"],
+            ["Airi", "Satou", "Accountant", search_value, "28th Nov 08", "$162,700"],
             ["Angelica", "Ramos", "Chief Executive Officer (CEO)", "London", "9th Oct 09", "$1,200,000"]
         ]
     }
